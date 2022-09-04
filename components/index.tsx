e5a6c2ad2167f3ed209a28430d078c37fd8d4442
Adi-31 <mail4adithya31@gmail.com>
2022-09-04 15:40:21 +0530

    initial build

---
+++ components/index.tsx
@@ -0,0 +1,25 @@
+import Banner from './Banner'
+import Header from './Header'
+import BasicMenu from './BasicMenu'
+import Loader from './Loader'
+import Login from './Login'
+import Membership from './Membership'
+import Modal from './Modal'
+import Plans from './Plans'
+import Row from './Row'
+import Table from './Table'
+import Thumbnail from './Thumbnail'
+
+export {
+      Banner,
+      Header,
+      BasicMenu,
+      Loader,
+      Login,
+      Membership,
+      Modal,
+      Plans,
+      Row,
+      Table,
+      Thumbnail
+}
